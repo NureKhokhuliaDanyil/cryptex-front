3fe3f9a140e814d21282016491af499c543c9162
Daniil Khokhulia <danyil.khokhulia@nure.ua>
2025-06-02 09:33:50 +0200

    Change routes for hosting

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const getUserByIdAsync = async (userId: string, token: string) => {
-    const response = await axios.get(`http://localhost:7072/api/user/${userId}`, { headers: { Authorization: token } }, );
+    const response = await axios.get(`https://cryptexapi.azurewebsites.net//api/user/${userId}`, { headers: { Authorization: token } }, );
     return response.data;
 }
 
 export const changeUserBalanceAsync = async (userId: string, balance: number, token: string) : Promise<void> => {
-    const response = await axios.patch(`http://localhost:7072/api/user/${userId}/change-balance?balance=${balance}`, {}, { headers: { "Authorization": token } });
+    const response = await axios.patch(`https://cryptexapi.azurewebsites.net//api/user/${userId}/change-balance?balance=${balance}`, {}, { headers: { "Authorization": token } });
     return response.data;
 }
